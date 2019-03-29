0210aa9b5d2b2383bb38e6c98edc2834b36c6e9d
NgoQuan <quan.asc.xda.k56pm@gmail.com>
2019-03-30 02:01:41 +0700

    Writing your first Django app, part 7

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,6 +5,7 @@ from .models import Question, Choice
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 # def index(request):
@@ -35,34 +36,41 @@ from django.views import generic
 #     # response = "You're looking at the results of question %s"
 #     # return HttpResponse(response % question_id)
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()
+                                       ).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
         return render(request, "polls/detail.html", {
-            'question' : question,
-            'error_message' : "You didn't select a choice.",})
+            'question': question,
+            'error_message': "You didn't select a choice.", })
     else:
         selected_choice.votes = F('votes') + 1
         selected_choice.save()
 
-    return HttpResponseRedirect(reverse('polls:results', args = (question_id, )))
+    return HttpResponseRedirect(reverse('polls:results', args=(question_id, )))
 
     # return HttpResponse("You're votting on question %s" % question_id)
-
